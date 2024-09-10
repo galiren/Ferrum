766cbaee7d6096d2aea7b80c191ab3ddf1d30162
galiren <moonexiler@gmail.com>
2024-09-10 18:38:30 +0800

    update: SDK version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
   namespace = "com.galiren.ferrum"
-  compileSdk = 34
+  compileSdk = 35
 
   defaultConfig {
     applicationId = "com.galiren.ferrum"
     minSdk = 24
-    targetSdk = 34
+    targetSdk = 35
     versionCode = 1
     versionName = "1.0"
 
